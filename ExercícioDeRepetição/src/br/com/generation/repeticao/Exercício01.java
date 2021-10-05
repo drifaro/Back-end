e0c88903bf7836ff47858d45f2d6010bd9ef04a1
Drislaine Faro <54925297+drifaro@users.noreply.github.com>
2021-10-05 14:41:37 -0300

    exercícios de repetição

---
+++ ExercícioDeRepetição/src/br/com/generation/repeticao/Exercício01.java
@@ -0,0 +1,17 @@
+package br.com.generation.repeticao;
+
+public class Exercício01 {
+
+	public static void main(String[] args) {
+	
+		int i;
+		
+		for (i = 1000; i <=1999; i++) {
+			
+			if(i % 11 ==5) {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
